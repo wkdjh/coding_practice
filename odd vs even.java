6c48ec7c29e83382db091eb07963c453da077e73
jh <wkdjh1019@gmail.com>
2025-05-01 16:49:08 +0900

    홀수 vs 짝수

---
+++ odd vs even.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        int even_sum = 0;
+        int odd_sum = 0;
+        
+        for (int i=0; i<num_list.length; i++){
+            if(i % 2 ==0)
+            {
+                odd_sum += num_list[i];
+            }
+            else {
+                even_sum += num_list[i];
+            }
+        }      
+        
+        return (even_sum > odd_sum) ? even_sum : odd_sum;
+    }
+}
